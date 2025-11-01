1641024f3b7d4046ae4e702eb34c156bda29f0f1
KEMAFUJA <kevin_mfuentes98@hotmail.com>
2025-11-01 18:25:46 -0400

    Update particulas_suben.js

--- -OTROS/particulas_suben.js
+++ -OTROS/particulas_suben.js
@@ -4,7 +4,8 @@ export function createGoldParticles(containerSelector, {
     cuantas = 40,
     tamMax = 50,
     durMax = 20,
-    aparMax = 20
+    aparMax = 20,
+    tamañoIgual = true // nuevo parámetro: true = width=height, false = diferentes
 } = {}) {
     const container = document.querySelector(containerSelector);
     if (!container) return;
@@ -21,11 +22,17 @@ export function createGoldParticles(containerSelector, {
         particle.style.top = `100%`;
 
         // tamaño aleatorio
-        particle.style.width = `${Math.random() * tamMax}px`;
-        particle.style.height = `${Math.random() * tamMax}px`;
+        if (tamañoIgual) {
+            const size = Math.random() * tamMax;
+            particle.style.width = `${size}px`;
+            particle.style.height = `${size}px`;
+        } else {
+            particle.style.width = `${Math.random() * tamMax}px`;
+            particle.style.height = `${Math.random() * tamMax}px`;
+        }
 
         // duración aleatoria
-        const duration = Math.random() * durMax + 10; // ajustable
+        const duration = Math.random() * durMax + 30; // ajustable
         particle.style.animationDuration = `${duration}s`;
 
         // retardo aleatorio
