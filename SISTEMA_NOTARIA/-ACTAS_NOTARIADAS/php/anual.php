37cdd6c4fd61b9ca7322ad41ac8379ae23031dd5
KEMAFUJA <kevin_mfuentes98@hotmail.com>
2026-05-16 10:51:14 -0400

    Add files via upload

---
+++ SISTEMA_NOTARIA/-ACTAS_NOTARIADAS/php/anual.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../../conexion.php';
+$pdo = Database::connect();
+
+$sql = "
+SELECT 
+    CAST(split_part(d.numero, '/', 2) AS INT) as anio,
+    COUNT(*) as cantidad
+FROM documento d
+JOIN tipo_documento td 
+    ON td.id_tipo = d.id_tipo
+WHERE td.nombre = 'ACTAS NOTARIADAS'
+GROUP BY anio
+ORDER BY anio;
+";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
